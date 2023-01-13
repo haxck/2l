337278469bae0ea317b4f194713c61bf96e0a763
Zhiqiang Du <haxck@qq.com>
2023-01-13 15:28:36 +0800

    后台api接口

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,66 @@
-// const cc = require('./mongoDB').getCollection("lovewords")
-
-
-// const col =await cc();
-// const reslut = col.find({}).toArray()
-// console.log(reslut)
-
-
 const express = require("express")
 const app = express()
 const config = require("./config.json")
-const lovewords =require('./models/lovewords')
+const lovewords = require('./models/lovewords')
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5501');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
+
 
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-  res.send("hello");
+app.get("/:id?", async (req, res) => {
+  console.log(req.params.id);
+  if (!req.params.id) {
+    try {
+      const words = await lovewords.random(1)
+      res.json(words);
+    } catch (error) {
+      console.log(error)
+      res.status(404).send()
+    }
+  }
+  try {
+    const words = await lovewords.findOne(req.params.id)
+    res.json(words);
+  } catch (error) {
+    console.log(error)
+    res.status(404).send()
+  }
+
 })
 
-app.get("/test",async (req,res)=>{
+app.get("/m/:c?", async (req, res) => {
+  let size
+  if (!req.params.c) {
+    size = 1
+  } else {
+    size = parseInt(req.params.c)
+  }
   try {
-    const words = await lovewords.findAll()
+    const words = await lovewords.random(size > 5 ? 5 : 1)
+    res.json(words);
+  } catch (error) {
+    console.log(error)
+    res.status(404).send()
+  }
+})
+app.post("/like", async (req, res) => {
+  try {
+    const words = await lovewords.like(req.body.id)
+    res.json(words);
+  } catch (error) {
+    console.log(error)
+    res.status(404).send()
+  }
+})
+app.post("/unlike", async (req, res) => {
+  try {
+    const words = await lovewords.dislike(req.body.id)
     res.json(words);
   } catch (error) {
     console.log(error)
@@ -27,6 +68,6 @@ app.get("/test",async (req,res)=>{
   }
 })
 
-app.listen(config.port,()=>{
+app.listen(config.port, () => {
   console.log("api is running")
 })
--- backend/models/lovewords.js
+++ backend/models/lovewords.js
@@ -1,10 +1,56 @@
 const lovewords = require('../mongoDB').getCollection("lovewords")
-
-exports.findAll = async () =>{
+const { ObjectId } = require("mongodb");
+exports.random = async (size) => {
+  console.log(size)
   try {
     const col = await lovewords()
-    return col.aggregate( [ { $sample: { size: 1 } } ] ).toArray();
+    return col.aggregate([{ $sample: { size: parseInt(size) } }]).toArray();
   } catch (error) {
     throw "查询失败"
   }
 }
+
+exports.findOne = async (id) => {
+  try {
+    const col = await lovewords()
+    return col.find({ _id: ObjectId(id) }).toArray();
+  } catch (error) {
+    throw "查询失败"
+  }
+}
+
+exports.like = async (id) => {
+  try {
+    const col = await lovewords();
+    const result = await col.findOneAndUpdate(
+      { _id: ObjectId(id) },
+      {
+        $inc: {
+          "likeCount": 1
+        }
+      },
+      { returnOriginal: false }
+    );
+    return result.value;
+  } catch (error) {
+    throw "喜欢失败";
+  }
+};
+
+exports.dislike = async (id) => {
+  try {
+    const col = await lovewords();
+    const result = await col.findOneAndUpdate(
+      { _id: ObjectId(id) },
+      {
+        $inc: {
+          "likeCount": -1
+        }
+      },
+      { returnOriginal: false }
+    );
+    return result.value;
+  } catch (error) {
+    throw "取消喜欢失败";
+  }
+};
