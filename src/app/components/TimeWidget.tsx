cfa14f1482109a892a6dbf0fc98666d341530081
haxck <haxck@qq.com>
2023-07-01 19:24:21 +0800

    优化字体颜色

---
+++ src/app/components/TimeWidget.tsx
@@ -0,0 +1,38 @@
+export default function TimeWidget() {
+    const today = new Date();
+    function welcome() {
+        const hours = today.getHours() + 1;
+        if (hours >= 0 && hours <= 5) {
+            return "夜深了，你还在思考宇宙嘛？"
+        } else if (hours >= 6 && hours <= 8) {
+            return "早啊，新的一天来咯，向快乐出发！"
+        } else if (hours >= 9 && hours <= 11) {
+            return "阳光明媚，加油加油！"
+        } else if (hours >= 12 && hours <= 14) {
+            return "中午啦，补充点能力，休息一下！"
+        } else if (hours >= 15 && hours <= 18) {
+            return "未来可期！加油加油！"
+        } else if (hours >= 19 && hours <= 21) {
+            return "晚上好，你也发愁吃点什么嘛？"
+        } else if (hours >= 22 && hours <= 24) {
+            return "🛌夜深了,该睡觉啦，枕着甜甜的美梦入睡哈~"
+        }
+    }
+    function s() {
+        const week = today.getDay()
+        if ((week-5)>0 || week == 0){
+            return "🎉今天休息啦🎉"
+        }else if((week-5)==0){
+            return "明天就是周六啦"
+        }else if((week-5)<0){
+            return "还有" +Math.abs(week-5) +"就可以休息啦"
+        }
+    }
+    return (
+        <div className="words isolate rounded-xl border border-gray-600/10 p-4 shadow-xl shadow-gray-400/10 transition-all duration-300 dark:shadow-black/0 flex flex-col justify-center bg-orange-100/20 sm:col-span-6 lg:col-span-4 my-4 inline-block">
+            <p className="text-gray-400">📅 {today.getMonth() + 1}月{today.getDate()}日 </p>
+            <p className="text-xl">{s()}</p>
+            <p>{welcome()}</p>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import WordWidget from './components/WordWidget'
 import FoodWidget from './components/FoodWidget'
+import TimeWidget from './components/TimeWidget'
 async function getWord() {
   const res = await fetch(`http://localhost:3000/api`, { cache: 'no-store' })
   return res.json()
@@ -10,15 +11,13 @@ export default async function Home() {
 
   const word = await getWord()
   return (
-    <div className="p-6 select-none flex flex-col justify-center h-screen">
-
+    <div className="p-6 select-none h-screen">
+      <TimeWidget />
       <WordWidget content={word[0]} />
       <div className='grid grid-cols-2 gap-3'>
-
         <FoodWidget list={{ promt: "一会儿去吃？", list: "盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 水饺 日本料理 涮羊肉 味千拉面 面包 扬州炒饭 自助餐 菜饭骨头汤 茶餐厅 海底捞 西贝莜面 披萨 麦当劳 KFC 汉堡王 卡乐星 兰州拉面 沙县小吃 烤鱼 烤肉 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 全家便当" }} />
         <FoodWidget list={{ promt: "猜丁壳", list: "✊ ✌ 👋 ✊ ✌ 👋 ✊ ✌ 👋 ✊ ✌ 👋" }} />
       </div>
-
     </div>
   )
 }
