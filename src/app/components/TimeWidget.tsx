9a491541a7ef212027eb265e0e88449f53871b82
Zhiqiang Du <haxck@qq.com>
2023-07-05 13:02:38 +0800

    修改倒计时文字

--- src/app/components/TimeWidget.tsx
+++ src/app/components/TimeWidget.tsx
@@ -25,14 +25,14 @@ export default function TimeWidget() {
         }else if((week-5)==0){
             return "明天就是周六啦"
         }else if((week-5)<0){
-            return "还有" +Math.abs(week-5) +"就可以休息啦"
+            return "还有 " +Math.abs(week-5) +" 天就休息啦"
         }
     }
     return (
-        <div className="words isolate rounded-xl border border-gray-600/10 p-4 shadow-xl shadow-gray-400/10 transition-all duration-300 dark:shadow-black/0 flex flex-col justify-center bg-orange-100/20 sm:col-span-6 lg:col-span-4 my-4 inline-block space-y-1">
-            <p className="text-gray-400">📅 {today.getMonth() + 1}月{today.getDate()}日 </p>
-            <p className="text-xl">{s()}</p>
-            <p>{welcome()}</p>
+        <div className="words dark:bg-slate-800  bg-slate-100 opacity-70 rounded-xl border border-gray-600/10 p-4 shadow-xl shadow-gray-400/10 transition-all duration-300 dark:shadow-black/0 flex flex-col justify-center bg-orange-100/20 sm:col-span-6 lg:col-span-4 my-4 inline-block space-y-1">
+            <p className="text-gray-400 ">📅 {today.getMonth() + 1}月{today.getDate()}日 </p>
+            <p className="text-xl dark:text-slate-400">{s()}</p>
+            <p className="dark:text-slate-400">{welcome()}</p>
         </div>
     )
 }
