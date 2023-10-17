3d95a0a86af2cd39f90bcef8995d8c0e632db297
Zhiqiang Du <haxck@qq.com>
2023-10-17 16:46:36 +0800

    调整首页布局

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,12 @@ const WordWidget = dynamic(() => import('./components/WordWidget'), {
 import FoodWidget from './components/FoodWidget'
 import TimeWidget from './components/TimeWidget'
 import Link from './components/LinkWidget'
+let url = ""
+if(window !== undefined ){
+  url = window.location.origin + "/api"
+}
 async function getWord() {
-  const res = await fetch(`http://localhost:3000/api`, { cache: 'no-store' })
+  const res = await fetch(url, { cache: 'no-store' })
   return res.json()
 }
 
@@ -26,14 +30,17 @@ export default async function Home() {
 
   const word = await getWord()
   return (
-    <div className="p-6 select-none min-h-screen bg">
-      <TimeWidget />
-      <WordWidget content={word[0]} />
-      <div className='grid grid-cols-2 gap-3'>
-        <FoodWidget list={{ promt: "一会儿去吃", list: "什么呢？ 盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 水饺 日本料理 涮羊肉 味千拉面 面包 扬州炒饭 自助餐 菜饭骨头汤 茶餐厅 海底捞 西贝莜面 披萨 麦当劳 KFC 汉堡王 卡乐星 兰州拉面 沙县小吃 烤鱼 烤肉 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 全家便当" }} />
-        <FoodWidget list={{ promt: "猜丁壳", list: "一局定胜负 ✌ 👋 ✊" }} />
-        <Link />
+    <div className="p-6 select-none min-h-screen bg flex justify-center items-center">
+      <div className='max-sm:w-full max-lg:w-2/3 max-xl:w-3/4 w-1/4'>
+        <TimeWidget />
+        <WordWidget content={word[0]} />
+        <div className='grid grid-cols-2 gap-3'>
+          <FoodWidget list={{ promt: "一会儿去吃", list: "什么呢？ 盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 水饺 日本料理 涮羊肉 味千拉面 面包 扬州炒饭 自助餐 菜饭骨头汤 茶餐厅 海底捞 西贝莜面 披萨 麦当劳 KFC 汉堡王 卡乐星 兰州拉面 沙县小吃 烤鱼 烤肉 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 全家便当" }} />
+          <FoodWidget list={{ promt: "猜丁壳", list: "一局定胜负 ✌ 👋 ✊" }} />
+          <Link />
+        </div>
       </div>
+
     </div>
   )
 }
