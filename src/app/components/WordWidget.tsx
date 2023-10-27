40e794abe771b767f2fcffd0e4513d241c0b0ea3
Zhiqiang Du <haxck@qq.com>
2023-10-27 13:12:48 +0800

    add loading animation in wordComponent

--- src/app/components/WordWidget.tsx
+++ src/app/components/WordWidget.tsx
@@ -2,32 +2,30 @@
 import { useEffect, useState } from "react"
 import { server } from "../config"
 
-interface content {
-  _id: string,
-  type: string,
-  sentent: string,
-  likeCount: number
-}
-
-export default function Word({ content }: {content: content}) {
+export default function Word() {
   // 先获取之前是否点过赞
-  let cacheLike;
+
+  const [data, setData] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  const [isLike, setIsLike] = useState(false)
+
   useEffect(() => {
-    cacheLike = localStorage.getItem(content._id)
+    fetch(`${server}/api`)
+      .then((res) => res.json())
+      .then(data => {
+        setData(data[0])
+        setIsLoading(false)
+        setIsLike(Boolean(localStorage.getItem(data[0]._id)))
+      })
+
   }, [])
 
-  const [isLike, setIsLike] = useState(Boolean(cacheLike))
-  const [data, setData] = useState({
-    ...content
-  })
 
   const fetchWord = async () => {
     const res = await fetch(`${server}/api`)
     const data = await res.json();
-    const word = data[0]
-    setData(word)
-    const cacheLike = localStorage.getItem(word._id)
-    setIsLike(Boolean(cacheLike))
+    setData(data[0])
+    setIsLike(Boolean(localStorage.getItem(data[0]._id)))
   }
 
   async function likeit() {
@@ -73,8 +71,22 @@ export default function Word({ content }: {content: content}) {
     }
   }
 
+  if (isLoading) return <>
+      <div className="animate-pulse isolate rounded-xl border border-gray-600/10 p-4 shadow-xl shadow-gray-400/10 transition-all duration-300 dark:shadow-black/0 flex flex-col justify-between  bg-red-100/20 px-4 sm:col-span-6 lg:col-span-4">
+      <div className="flex-1 space-y-6 py-1">
+        <div className="h-3 bg-slate-200 rounded"></div>
+        <div className="space-y-3">
+          <div className="h-3 bg-slate-200 rounded"></div>
+        </div>
+      </div>
+    </div>
+  </>
+  if (!data) return <p>No profile data</p>
+
+
+
   return (
-    <div className="dark:bg-slate-800 bg-slate-100 opacity-70 rounded-xl border border-gray-600/10 p-4 shadow-xl shadow-gray-400/10 transition-all duration-300 dark:shadow-black/0 flex flex-col justify-between  bg-red-100/20 px-4 sm:col-span-6 lg:col-span-4">
+    <div className="dark:bg-slate-800 bg-slate-100 opacity-90 rounded-xl border border-gray-600/10 p-4 shadow-xl shadow-gray-400/10 transition-all duration-300 dark:shadow-black/0 flex flex-col justify-between  bg-red-100/20 px-4 sm:col-span-6 lg:col-span-4">
 
       <div className="before:block before:absolute before:-inset-1 before:-skew-y-3 before: relative block p-2 " >
         <p className="rise-up bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent relative words text-xl whitespace-pre-wrap">
@@ -95,4 +107,9 @@ export default function Word({ content }: {content: content}) {
       </div>
     </div>
   )
+
+
+
+
+
 }
